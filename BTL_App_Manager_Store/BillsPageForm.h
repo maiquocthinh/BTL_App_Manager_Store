ee2b6f5869aecdc4841f0ad397b4039572dc905d
ngogiahuyk61 <ngogiahuy.utc2@gmail.com>
2022-09-29 16:42:25 +0700

    Xây dựng giao diện cho BillsPageForm.h

--- BTL_App_Manager_Store/BillsPageForm.h
+++ BTL_App_Manager_Store/BillsPageForm.h
@@ -34,7 +34,21 @@ namespace BTLAppManagerStore {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	protected:
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel2;
+	private: System::Windows::Forms::TextBox^ tbxSearch;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Button^ btnRefresh;
+	private: System::Windows::Forms::Button^ btnNewBill;
+	private: System::Windows::Forms::Button^ btnSearch;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ billID;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ billNameCustomer;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ billDate;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ billTotalMoney;
+
 	protected:
 
 	private:
@@ -50,30 +64,213 @@ namespace BTLAppManagerStore {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->tbxSearch = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->btnRefresh = (gcnew System::Windows::Forms::Button());
+			this->btnNewBill = (gcnew System::Windows::Forms::Button());
+			this->btnSearch = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->billID = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->billNameCustomer = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->billDate = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->billTotalMoney = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->tableLayoutPanel2->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// label1
+			// tableLayoutPanel1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->tableLayoutPanel1->ColumnCount = 1;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				100)));
+			this->tableLayoutPanel1->Controls->Add(this->tableLayoutPanel2, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->dataGridView1, 0, 1);
+			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 2;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 80)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 100)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1207, 651);
+			this->tableLayoutPanel1->TabIndex = 1;
+			// 
+			// tableLayoutPanel2
+			// 
+			this->tableLayoutPanel2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel2->ColumnCount = 5;
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				170)));
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				100)));
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				160)));
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				160)));
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				200)));
+			this->tableLayoutPanel2->Controls->Add(this->tbxSearch, 0, 0);
+			this->tableLayoutPanel2->Controls->Add(this->groupBox1, 0, 0);
+			this->tableLayoutPanel2->Controls->Add(this->btnRefresh, 3, 0);
+			this->tableLayoutPanel2->Controls->Add(this->btnNewBill, 3, 0);
+			this->tableLayoutPanel2->Controls->Add(this->btnSearch, 2, 0);
+			this->tableLayoutPanel2->Location = System::Drawing::Point(3, 3);
+			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
+			this->tableLayoutPanel2->RowCount = 1;
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 100)));
+			this->tableLayoutPanel2->Size = System::Drawing::Size(1201, 74);
+			this->tableLayoutPanel2->TabIndex = 0;
+			// 
+			// tbxSearch
+			// 
+			this->tbxSearch->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tbxSearch->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(12, 9);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(217, 52);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"Bills Page";
+			this->tbxSearch->Location = System::Drawing::Point(188, 14);
+			this->tbxSearch->Margin = System::Windows::Forms::Padding(18, 14, 18, 18);
+			this->tbxSearch->Name = L"tbxSearch";
+			this->tbxSearch->Size = System::Drawing::Size(475, 38);
+			this->tbxSearch->TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->groupBox1->Controls->Add(this->comboBox1);
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->groupBox1->Location = System::Drawing::Point(3, 3);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(164, 68);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Search";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"ID", L"Name Customer" });
+			this->comboBox1->Location = System::Drawing::Point(6, 21);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(132, 26);
+			this->comboBox1->TabIndex = 0;
+			// 
+			// btnRefresh
+			// 
+			this->btnRefresh->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnRefresh->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnRefresh->Location = System::Drawing::Point(853, 12);
+			this->btnRefresh->Margin = System::Windows::Forms::Padding(12);
+			this->btnRefresh->Name = L"btnRefresh";
+			this->btnRefresh->Size = System::Drawing::Size(136, 50);
+			this->btnRefresh->TabIndex = 4;
+			this->btnRefresh->Text = L"Refresh";
+			this->btnRefresh->UseVisualStyleBackColor = true;
+			// 
+			// btnNewBill
+			// 
+			this->btnNewBill->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnNewBill->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnNewBill->Location = System::Drawing::Point(1013, 12);
+			this->btnNewBill->Margin = System::Windows::Forms::Padding(12);
+			this->btnNewBill->Name = L"btnNewBill";
+			this->btnNewBill->Size = System::Drawing::Size(176, 50);
+			this->btnNewBill->TabIndex = 5;
+			this->btnNewBill->Text = L"New Bill";
+			this->btnNewBill->UseVisualStyleBackColor = true;
+			// 
+			// btnSearch
+			// 
+			this->btnSearch->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnSearch->Font = (gcnew System::Drawing::Font(L"Berlin Sans FB", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnSearch->Location = System::Drawing::Point(693, 12);
+			this->btnSearch->Margin = System::Windows::Forms::Padding(12);
+			this->btnSearch->Name = L"btnSearch";
+			this->btnSearch->Size = System::Drawing::Size(136, 50);
+			this->btnSearch->TabIndex = 3;
+			this->btnSearch->Text = L"Search";
+			this->btnSearch->UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
+				this->billID,
+					this->billNameCustomer, this->billDate, this->billTotalMoney
+			});
+			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->dataGridView1->Location = System::Drawing::Point(3, 83);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->RowHeadersWidth = 62;
+			this->dataGridView1->RowTemplate->Height = 28;
+			this->dataGridView1->Size = System::Drawing::Size(1201, 565);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// billID
+			// 
+			this->billID->HeaderText = L"ID";
+			this->billID->MinimumWidth = 8;
+			this->billID->Name = L"billID";
+			this->billID->ReadOnly = true;
+			// 
+			// billNameCustomer
+			// 
+			this->billNameCustomer->HeaderText = L"Name Customer";
+			this->billNameCustomer->MinimumWidth = 8;
+			this->billNameCustomer->Name = L"billNameCustomer";
+			this->billNameCustomer->ReadOnly = true;
+			// 
+			// billDate
+			// 
+			this->billDate->HeaderText = L"Date";
+			this->billDate->MinimumWidth = 8;
+			this->billDate->Name = L"billDate";
+			this->billDate->ReadOnly = true;
+			// 
+			// billTotalMoney
+			// 
+			this->billTotalMoney->HeaderText = L"Total Money";
+			this->billTotalMoney->MinimumWidth = 8;
+			this->billTotalMoney->Name = L"billTotalMoney";
+			this->billTotalMoney->ReadOnly = true;
 			// 
 			// BillsPageForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1232, 696);
-			this->Controls->Add(this->label1);
+			this->ClientSize = System::Drawing::Size(1207, 651);
+			this->Controls->Add(this->tableLayoutPanel1);
 			this->Name = L"BillsPageForm";
 			this->Text = L"BillsPageForm";
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel2->ResumeLayout(false);
+			this->tableLayoutPanel2->PerformLayout();
+			this->groupBox1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
